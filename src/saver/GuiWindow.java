3b3b995ec541cffa71793374cebf6229c3d20e2d
Sam Luby <samwluby@gmail.com>
2017-07-10 17:26:22 +0100

    GUI window

---
+++ src/saver/GuiWindow.java
@@ -0,0 +1,163 @@
+package saver;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.Font;
+
+public class GuiWindow {
+	public static class Params {
+		public final String sourcePath;
+		public final String destinationPath;
+		
+		public Params(String sourcePath, String destinationPath) {
+			this.sourcePath = sourcePath;
+			this.destinationPath = destinationPath;
+		}
+	}
+	
+	//New class CloseableJDialog
+	static class CloseableJDialog extends JDialog {
+		private static final long serialVersionUID = 1L;
+
+		public CloseableJDialog() {
+			setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+			addWindowListener(new WindowAdapter() {
+				@Override
+				public void windowClosing(WindowEvent e) {
+					System.exit(0);
+				}
+			});
+		}
+	}
+
+	//Dialog dimensions
+	private final int buttonWidth = 100;
+	private final int buttonHeight = 25;
+	private final int fieldWidth = 300;
+	private final int fieldHeight = 25;
+	private final int windowWidth = 600;
+	private final int windowHeight = 200;
+	private final int row0 = 10;
+	private final int row1 = 40;
+	private final int row2 = 120;
+	private final int col0 = 10;
+	private final int col1 = 140;
+	private final int col2 = 450;
+	
+	private CloseableJDialog dialog;
+	private JButton sourceBrowse;
+	private JButton destinationBrowse;
+	private JButton runButton;
+	private JButton cancelButton;
+	private JLabel sourceFolderLabel;
+	private JLabel destinationFolderLabel;
+	private JTextField sourceFolderField;
+	private JTextField destinationFolderField;
+	private JFileChooser choseFolder = new JFileChooser();
+	
+	
+	public void showGui(String sourePath) {
+		dialog = new CloseableJDialog();
+		dialog.setBounds(100, 100, windowWidth, windowHeight);
+		dialog.setLocationRelativeTo(null);
+		dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		Container contentPane = dialog.getContentPane();
+		contentPane.setLayout(null);
+		dialog.setResizable(false);
+		dialog.setTitle("Spotlight Saver");
+		Font textFont = new Font("SansSerif", Font.PLAIN, 14);
+		
+		//Source
+		sourceFolderLabel = new JLabel("Source Folder:");
+		sourceFolderLabel.setBounds(col0, row0, fieldWidth, fieldHeight);
+		sourceFolderLabel.setFont(textFont);
+		contentPane.add(sourceFolderLabel);
+		
+		sourceFolderField = new JTextField();
+		sourceFolderField.setBounds(col1, row0, fieldWidth, fieldHeight);
+		sourceFolderField.setFont(textFont);
+		contentPane.add(sourceFolderField);
+		
+		sourceBrowse = new JButton("Browse");
+		sourceBrowse.setToolTipText("Search for source folder");
+		sourceBrowse.setBounds(col2, row0, buttonWidth, buttonHeight);
+		contentPane.add(sourceBrowse);
+		
+		//Destination
+		destinationFolderLabel = new JLabel("Destination Folder:");
+		destinationFolderLabel.setBounds(col0, row1, fieldWidth, fieldHeight);
+		destinationFolderLabel.setFont(textFont);
+		contentPane.add(destinationFolderLabel);
+		
+		destinationFolderField = new JTextField();
+		destinationFolderField.setBounds(col1, row1, fieldWidth, fieldHeight);
+		destinationFolderField.setFont(textFont);
+		contentPane.add(destinationFolderField);
+		
+		destinationBrowse = new JButton("Browse");
+		destinationBrowse.setToolTipText("Search for destination folder");
+		destinationBrowse.setBounds(col2, row1, buttonWidth, buttonHeight);
+		contentPane.add(destinationBrowse);
+
+
+		//Run & Cancel Buttons
+		runButton = new JButton("Run");
+		runButton.setToolTipText("Run the program");
+		runButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dialog.dispose();
+			}
+		});
+		runButton.setBounds(col0, row2, buttonWidth, buttonHeight);
+		contentPane.add(runButton);
+		
+		cancelButton = new JButton("Cancel");
+		cancelButton.setToolTipText("Cancel program");
+		cancelButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		cancelButton.setBounds(col2, row2, buttonWidth, buttonHeight);
+		contentPane.add(cancelButton);
+		
+		
+		addButtonListener(sourceBrowse, sourceFolderField, "Source Folder");
+		addButtonListener(destinationBrowse, destinationFolderField, "Destination Folder");
+		
+		contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setFont(textFont);
+		dialog.setVisible(true);
+	}
+	
+	private void addButtonListener(JButton button, final JTextField field, final String description) {
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Component frame = null;
+				int returnVal = choseFolder.showOpenDialog(frame);
+				if(returnVal == JFileChooser.APPROVE_OPTION) {
+					field.setText(choseFolder.getSelectedFile().toString());
+				}
+			}
+		});
+	}
+	
+	public Params getParams() {
+		return new Params(sourceFolderField.getText(), destinationFolderField.getText());
+	}
+}
---
+++ src/saver/SpotlightSaver.java
@@ -0,0 +1,131 @@
+package saver;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+import saver.GuiWindow.Params;
+
+public class SpotlightSaver {
+	
+	private static SpotlightSaver save;
+//	static String src = "C:\\Users\\samwl\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets";
+//	static String src = "";
+//	static String dest = "C:\\Users\\samwl\\Documents\\SpotlightPics";
+	private static String sourceFolder = "";
+	private static String destinationFolder = "";
+	
+	private SpotlightSaver() {
+	}
+	
+    public static void main(String[] args) {
+    	try {
+	    	Params params = null;
+	    	params = launchWindow(sourceFolder, destinationFolder);
+	    	
+	    	save = new SpotlightSaver();
+	    	List<File> files = parseParams(params);
+
+	        copyFolder(files.get(0), files.get(1));
+	        deleteSmallShit(files.get(1));
+	        changeExt(files.get(1));   
+	        
+    	} catch (Throwable t) {
+    		System.out.println("Terminated unexpectedly");
+    		t.printStackTrace(System.out);
+    	}
+    }
+    
+    private static List<File> parseParams(Params params) {
+    	
+    	java.util.List<java.io.File> files = new ArrayList<>();
+    	if(params.sourcePath != null) {
+    		File sourceFolder = new File(params.sourcePath);
+    		files.add(sourceFolder);
+    	} else {
+    		JOptionPane.showMessageDialog(null, "No source folder given, program will exit.");
+    		System.exit(0);
+    	}
+    	if(params.destinationPath != null) {
+    		File destinationFolder = new File(params.destinationPath);
+    		files.add(destinationFolder);
+    	} else {
+    		JOptionPane.showMessageDialog(null, "No destination folder given, program will exit.");
+    		System.exit(0);
+    	}
+    	return files;
+    }
+    
+    /**
+     * This function recursively copy all the sub folder and files from sourceFolder to destinationFolder
+     * */
+    private static void copyFolder(File sourceFolder, File destinationFolder) throws IOException
+    {
+        //Check if sourceFolder is a directory or file
+        //If sourceFolder is file; then copy the file directly to new location
+        if (sourceFolder.isDirectory()) 
+        {
+            //Verify if destinationFolder is already present; If not then create it
+            if (!destinationFolder.exists()) 
+            {
+                destinationFolder.mkdir();
+                System.out.println("Directory created : " + destinationFolder);
+            }
+             
+            //Get all files from source directory
+            String files[] = sourceFolder.list();
+
+            //Iterate over all files and copy them to destinationFolder one by one
+            for (String file : files) 
+            {
+                File srcFile = new File(sourceFolder, file);
+                File destFile = new File(destinationFolder, file);
+                copyFolder(srcFile, destFile);
+            }
+        }
+        else
+        {
+            //Copy the file content from one place to another 
+            Files.copy(sourceFolder.toPath(), destinationFolder.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+    
+    public static void changeExt(File destinationFolder) {
+    	File[] list = destinationFolder.listFiles();
+    	
+    	for(int i=0; i < list.length; i++) {
+    		File f = new File("C:\\Users\\samwl\\Documents\\SpotlightPics\\"+list[i].getName());
+    		String newName = destinationFolder.toString()+"\\"+"pic_"+i+".png";
+    		f.renameTo(new File(newName));
+    	}
+    	System.out.println("All files converted");
+    }
+    
+    public static void deleteSmallShit(File destinationFolder) throws IOException {
+    	File[] list = destinationFolder.listFiles();
+    	for(File f : list) {
+    		if(f.length() < 200000) {
+    			f.delete();
+    		} 
+    	}
+    }
+    
+    private static Params launchWindow(final String src, final String dest) throws InvocationTargetException, InterruptedException {
+    	final GuiWindow window = new GuiWindow();
+    	SwingUtilities.invokeAndWait(new Runnable() {
+    		@Override
+    		public void run() {
+    			window.showGui(src);
+    		}
+    	});
+    	return window.getParams();
+    }
+}
